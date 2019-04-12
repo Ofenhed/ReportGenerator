33e5cdac2d0355d6c2b1a434ef49bbe8daba3d5c
Marcus Ofenhed <marcus@conditionraise.se>
2019-04-12 16:20:14 +0200

    Added unix time parsing to extended Ginger functions, and added missing library for previous commit.

--- src/GingerExtra.hs
+++ src/GingerExtra.hs
@@ -4,13 +4,17 @@
 module GingerExtra where
 import Types
 
-import Text.Ginger.GVal (fromFunction, Function(..), GVal(..), toGVal)
-import Text.Ginger.Run (Run(..))
+import Text.Ginger.GVal (fromFunction, Function(..), GVal(..), toGVal, keys)
+import Text.Ginger.Run (Run(..), liftRun)
 import Text.XML (parseText)
 import Text.Ginger (VarName(..))
 import Data.Default.Class
 import Data.Cvss.V3 (calculateEnvironmentalScore)
 import Data.Scientific (fromRationalRepetend)
+import Data.Time.LocalTime (utcToZonedTime, hoursToTimeZone)
+import Data.Time.Clock.System (SystemTime(MkSystemTime), systemToUTCTime)
+import Data.Scientific (toBoundedInteger)
+import System.IO.Unsafe (unsafePerformIO)
 import qualified Data.Text                      as Text
 import qualified Data.Text.Lazy                 as LText
 
@@ -23,6 +27,14 @@ parseCvss [(Nothing, cvss)] = return $ toGVal $ case filter (null . snd) $ reads
                                                                    in Just num
                                                   _ -> Nothing
 
+parsePosixTime [one_arg] = parsePosixTime [one_arg, (Nothing, toGVal 0)]
+parsePosixTime [(Nothing, time), (Nothing, offset)] =
+  return $ toGVal $ case (asNumber time, asNumber offset) of
+    (Just time', Just offset') -> case (toBoundedInteger time', toBoundedInteger offset') of
+                                    (Just time'', Just offset'') -> Just $ utcToZonedTime (hoursToTimeZone offset'') $ systemToUTCTime $ MkSystemTime time'' 0
+                                    _ -> Nothing
+    _ -> Nothing
+
 gingerFunctions :: Monad m => [(VarName, Function (Run p m a))]
 gingerFunctions = [("merge", \args -> return $ def { isNull = False
                                                    , asLookup = Just $ flip foldl (\_ -> Nothing)
@@ -32,4 +44,7 @@ gingerFunctions = [("merge", \args -> return $ def { isNull = False
                                                                                   args })
                   ,("parse_xml", parseXml)
                   ,("parse_cvss3", parseCvss)
-                  ,("none", (\[(Nothing, x)] -> return $ toGVal $ isNull x))]
+                  ,("none", \[(Nothing, x)] -> return $ toGVal $ isNull x)
+                  ,("raise_visible", \[(Nothing, x)] -> throw $ VisibleError $ asText x)
+                  ,("keys", \[(Nothing, x)] -> return $ toGVal $ keys x)
+                  ,("parse_posix_time", parsePosixTime)]
